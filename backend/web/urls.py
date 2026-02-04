1ec81e3734ece61d28ae8553ed4ffa75a55efb48
zixuantai <zixuantai@163.com>
2026-02-04 18:09:43 +0800

    实现角色增删改查后端api

--- backend/web/urls.py
+++ backend/web/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path, re_path
+
+from web.views.create.character.create import CreateCharacterView
+from web.views.create.character.get_single import GetSingleCharacterView
+from web.views.create.character.remove import RemoveCharacterView
+from web.views.create.character.update import UpdateCharacterView
 from web.views.index import index
 from web.views.user.account.get_user_info import GetUserInfo
 from web.views.user.account.login import LoginView
@@ -15,6 +20,10 @@ urlpatterns = [
     path('api/user/account/refresh_token/', RefreshTokenView.as_view()),
     path('api/user/account/get_user_info/', GetUserInfo.as_view()),
     path('api/user/profile/update/', UpdateProfileView.as_view()),
+    path('api/create/character/create/', CreateCharacterView.as_view()),
+    path('api/create/character/update/', UpdateCharacterView.as_view()),
+    path('api/create/character/remove/', RemoveCharacterView.as_view()),
+    path('api/create/character/get_single/', GetSingleCharacterView.as_view()),
     path('', index),
     re_path(r'^(?!media/|static/|assets/).*$', index),
 ]
