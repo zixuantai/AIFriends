0875b2f1b13da2e9c5c0687d2e1596e66a99d06b
zixuantai <zixuantai@163.com>
2026-02-12 00:30:06 +0800

    恢复角色卡片加载数量

--- backend/web/views/create/character/get_list.py
+++ backend/web/views/create/character/get_list.py
@@ -15,7 +15,7 @@ class GetListCharacterView(APIView):
             user_profile = UserProfile.objects.get(user=user)
             characters_raw = Character.objects.filter(
                 author=user_profile
-            ).order_by('-id')[items_count: items_count + 1]
+            ).order_by('-id')[items_count: items_count + 20]
             characters = []
             for character in characters_raw:
                 author = character.author
